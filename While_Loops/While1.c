c18952bdfc9c5f05a6872e39bbf44b1406c88831
leonemunyao <leonemunyao@gmail.com>
2023-09-10 14:56:13 +0300

    While loops

---
+++ While_Loops/While1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int i;
+i = 50;
+while (i < 60)
+{
+printf("The value of i is :%d\n", i);
+i++;
+}
+printf("\n");
+return (0);
+}
