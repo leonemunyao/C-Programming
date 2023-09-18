2d795a54f60602a3992569b2b0d135116d9aa7cf
leonemunyao <leonemunyao@gmail.com>
2023-09-18 11:13:07 +0300

    Pointers and Arrays

---
+++ Pointers_Arrays/0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - using sizeof to dynamically determine the size of types char, int and float
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int n;
+printf("Size of type 'char' in my computer: %lu bytes\n", sizeof(char));
+printf("Size of type 'int' in my computer: %lu bytes\n", sizeof(int));
+printf("Size of type 'float' in my computer: %lu bytes\n", sizeof(float));
+printf("Size of type my variable n on my computer: %lu bytes\n", sizeof(n));
+return (0);
+}
---
+++ Pointers_Arrays/1-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - addresses of the variable
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+char c;
+int n;
+printf("Address of the variable 'c': %p\n", &c);
+printf("Address of the variable 'n': %p\n", &n);
+return (0);
+}
