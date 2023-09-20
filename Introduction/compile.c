2cb61ee440a7fb05e9486e7f0cf6cb2177f44e4b
leonemunyao <leonemunyao@gmail.com>
2023-09-20 12:20:13 +0300

    Compilation

---
+++ Introduction/compile.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("The Compilation process in C follows the following steps: Preprocessor, Compiler, Assembler, and Linker. The preprocessor is the source file that is .c ...The compiler converts the code to assembly language...The assembler converts the assembly code to machine code...The Linker links the code to libraries\n");
+    return 0;
+}
